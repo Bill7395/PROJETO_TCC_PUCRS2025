1dcd100bf8d181e3de8a7e0b10f7cd07796681cb
Bill7395 <gabrielexecutive7395@gmail.com>
2025-06-19 21:14:17 -0300

    Dockerizacao

--- backend/server.js
+++ backend/server.js
@@ -6,23 +6,23 @@ const userRoutes = require('./routes/userRoutes');
 const productRoutes = require('./routes/productRoutes');
 const connectDB = require('./config/db');
 const paymentRoutes = require('./routes/paymentRoutes');
+const app = express();
+const orderRoutes = require('./routes/orderRoutes');
 
 dotenv.config();
 connectDB();
 
-const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3001',
+  credentials: true
+}));
 app.use(express.json());
-
 app.use('/api/users', userRoutes);
 app.use('/api/products', productRoutes);
+app.use('/api/orders', orderRoutes);
+app.use('/api/payment', paymentRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
-
-const orderRoutes = require('./routes/orderRoutes');
-app.use('/api/orders', orderRoutes);
-
-app.use('/api/payment', paymentRoutes);
 
 console.log('Variáveis de ambiente:', process.env.CLOUDINARY_CLOUD_NAME, process.env.CLOUDINARY_API_KEY);
